5e2c727504f37b80fc91f3a5b5bf0b063c285c12
Hodel <lhodel@dxc.com>
2019-11-26 12:27:57 +0100

    Pre-final

--- Check_Location/which_net.ps1
+++ Check_Location/which_net.ps1
@@ -4,7 +4,8 @@
         LHO 2019-11-18 V0.2 fine tuned using Rolfs Syntax input 
         LHO 2019-11-20 V0.3 Multiple ansewrs for defaultgateway => stronger selection
         LHO 2019-11-20 V0.4 Creating directory and files on first run. Writing to log file
-        LHo 2019-11-25 V0.5 Introduced Option Object to store different options
+        LHo 2019-11-25 V0.5 finetuned write to file, identifiy location from stored information 
+        LHo 2019-11-26 V0.6 minor glitches fixed 
 #>
 
 # Get-wmi Info and select nexthop ... most of the times being the default gateway => needs to vaildated in different environments
@@ -14,7 +15,7 @@
 # only the records matching the selection filter will be processd in Powershell => Less overhead => better performance 
 
 # Initialize stuff
-$new_Loc = $true
+$is_new_Loc = $true
 $Def_GW =""
 $loc_last = ""
 $loc_now = ""
@@ -35,18 +36,20 @@ function NOW {Get-Date -Format "dd.MM.yyyy @ HH:mm:ss:fff"}
 
 # Check if working path and files exist, if not create 
 
-if(-not (Test-Path $work_path)) { New-Item -Path $work_path -ItemType Directory}
-if(-not (Test-Path $w_log))     { New-Item -Path $w_log -ItemType "file"
+if(-not (Test-Path $work_path)) { New-Item -Path $work_path -ItemType Directory | out-null }
+if(-not (Test-Path $w_log))     { New-Item -Path $w_log -ItemType "file"  | out-null 
         (now)+" Logfile initialized" | Add-Content -Path $w_log
 }
-if(-not (Test-Path $w_last_location)) { New-Item -Path $w_last_location -ItemType "file"
-        #"none" | Add-Content -Path $w_last_location
+if(-not (Test-Path $w_last_location)) { New-Item -Path $w_last_location -ItemType "file"  | out-null 
+        (now)+" Last Location File initialized" | Add-Content -Path $w_log
 }
-if(-not (Test-Path $w_known_mac)){ New-Item -Path $w_known_mac -ItemType "file"}
-
+if(-not (Test-Path $w_known_mac)){ New-Item -Path $w_known_mac -ItemType "file"  | out-null 
+        (now)+" known MAC file initialized" | Add-Content -Path $w_log
+}
 # read last location
-
 $loc_last = Get-Content $w_last_location
+# read list of known macs
+$MACs = Get-Content $w_known_mac
 
 # confirm new scan to log file
 (NOW)+" **************** new scan ******************" | Add-Content -Path $w_log
@@ -69,29 +72,39 @@ foreach ($line in (arp.exe -a $Def_GW)) {
 }
 (now)+" This is what I recognize: Adapter:"+$Adapter+" My IP:"+$lcl_IP+" GW MAC:"+$MAC+" GW IP:"+$GWIP | Add-Content -Path $w_log
 
-# read list of known macs
-$MACs = Get-Content $w_known_mac
-
 foreach ($MAC_read in $MACs) {
         if($MAC_read.Split("")[0] -eq $MAC) {
                 $loc_now = $MAC_read.Split("")[1]
-                $new_Loc = $false
+                $is_new_Loc = $false
                 break
         }
 }
-
-if ($new_Loc){
-        (NOW)+" Did not recognise this location. Requesting a name:" | Add-Content -Path $w_log
-        Write-Host "What I see:`nUsing Adapter:" $Adapter "with IP:" $lcl_IP "we talk to a Gatway with MAC:" $MAC "and IP:" $GWIP "`nLooks like a new location.`n"        
-        $loc_now = read-Host "How do you want to name this location?"
-        $MAC+" "+$loc_now | Add-Content -Path $w_known_mac
-        (NOW)+" Added "+$MAC+" with Name: "+$loc_now+" to File known_mac.txt." | Add-Content -Path $w_log
-}
-
 if ($loc_now -eq $loc_last){
         (now)+" "+$loc_now+": Same location as before, exiting." | Add-Content -Path $w_log
-        break
-} else {
+        exit 
+}
+
+if ($is_new_Loc){
+        (NOW)+" Did not recognise this location. Requesting a name:" | Add-Content -Path $w_log
+        Write-Host "What I see:`nUsing Adapter:" $Adapter "with IP:" $lcl_IP "we talk to a Gatway with MAC:" $MAC "and IP:" $GWIP "`nLooks like a new location.`n"        
+        $loc_now = (read-Host "How do you want to name this location?").ToLower()
+        # writing new Mac and location name to file
+        $MAC+" "+$loc_now | Add-Content -Path $w_known_mac
+        (NOW)+" Added "+$MAC+" with Name: "+$loc_now+" to File known_mac.txt." | Add-Content -Path $w_log
+        # creating empty powershell script file
+        if (Test-Path  $($work_path+$loc_now+".ps1")) {
+                (NOW)+" Looks like a new def-gw in known env: "+$loc_now | Add-Content -Path $w_log
+        } else {
+                New-Item -Path $work_path$loc_now".ps1" -ItemType "file"
+                # add top comment to file and talk about it
+                "<#`n file performing actions in your "+$loc_now+" environment`n#>" | Add-Content -Path $work_path$loc_now".ps1"
+                Write-Host "created this options file for you:" $work_path$loc_now".ps1"
+                (NOW)+" optionsfile "+$work_path+$loc_now+".ps1 created" | Add-Content -Path $w_log
+        }
+        $loc_now | Set-Content -Path $w_last_location
+} 
+else {
         (now)+" Location has changed to "+$loc_now+", applying settings." | Add-Content -Path $w_log
         $loc_now | Set-Content -Path $w_last_location   
-}
+        Invoke-Expression $work_path$loc_now".ps1"
+        }
